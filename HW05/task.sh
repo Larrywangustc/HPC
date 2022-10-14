0208a312d4cac9d870b07df18bb18fa9d173f283
ZEYU WANG <zwang2832@euler-login-2.wacc.wisc.edu>
2022-10-14 17:53:07 -0500

    commit message

--- HW05/task.sh
+++ HW05/task.sh
@@ -10,15 +10,16 @@
 module load nvidia/cuda/11.6.0
 
 nvcc task1.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
-###for i in {10..30}
-###do
-###   n=$((2**i))
-###   ./task1 $n 16
-###done
+for i in {10..30}
+do
+   n=$((2**i))
+   ./task1 $n 16
+done
+
 
 nvcc task2.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task2
 for i in {5..14}
 do
    n=$((2**i))
-   ./task2 $n 16
+   ./task2 $n 32
 done
